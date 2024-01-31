4b94b795c8ecf8881ba13fda30a6b8e7bec30f74
tania.b <carolinaortz1@gmail.com>
2024-01-31 17:52:53 -0300

    se crea componente Portfolio Item

---
+++ data/portfolio.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        title: "Sitio 1",
+        imgUrl: "",
+        stack: ["HTML", "CSS"],
+        link: "",
+    },
+    {
+        title: "Sitio 2 CRUD",
+        imgUrl: "",
+        stack: ["HTML", "CSS", "Javascript"],
+        link: "https://tania-bobadilla.github.io/Ejercicio_CRUD/",
+    },
+    {
+        title: "Sitio 3 Dashboard",
+        imgUrl: "",
+        stack: ["HTML", "CSS", "Javascript"], 
+        link: "https://tania-bobadilla.github.io/Dashboard/",
+    },
+    {
+        title: "Sitio 4 Restaurant",
+        imgUrl: "",
+        stack: ["HTML", "CSS", "Javascript", "React"],
+        link: "https://restaurante-jsz4.vercel.app",
+    },
+    {
+        title: "Sitio 5 Surf",
+        imgUrl: "",
+        stack: ["HTML", "CSS", "Javascript", "React", "MongoDB"],
+        link: "https://fabulous-genie-b3ac69.netlify.app",
+    }
+
+]
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 function Intro () {
     return(
-        <div className="flex items-center justify-center flex-col text-center pt-20 pb-12">Intro</div>
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-12">
+            <h1 className="text-4xl md:text-7x1 mb-1 md:mb-3 font-bold">Tania Bobadilla</h1>
+            <p className="text-base md:text-xl mb-3 font-medium">Desarrolladora Web Fullstack</p>
+            <p className="text-md max-w-xl mb-6 font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid voluptatem repellendus officia quo sequi delectus quibusdam facilis at incidunt molestiae!</p>
+        </div>
+        
     )
 }
 
